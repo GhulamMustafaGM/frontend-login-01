1c31092a55b5bb4787a16512036cf92a2a7c3b05
Ghulam Mustafa <mustafaji@gmail.com>
2022-01-30 23:49:03 +0100

    Frontend Login Page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginPage1 from "./LoginPage1";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage1 />
+    </div>
+  );
+}
+
+export default App;
